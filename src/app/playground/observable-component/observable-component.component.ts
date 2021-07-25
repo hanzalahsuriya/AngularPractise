bc33fb02ee866e1b367f13c374782db8e9602c9e
hanzalah suriya <hanzalah@gmail.com>
2021-07-25 22:23:37 +0100

    adding multiple operator

--- src/app/playground/observable-component/observable-component.component.ts
+++ src/app/playground/observable-component/observable-component.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, ObservedValuesFromArray, Observer, Subscription } from 'rxjs';
-import { map } from 'rxjs/Operators';
+import { filter, map } from 'rxjs/Operators';
 
 @Component({
   selector: 'app-observable-component',
@@ -13,27 +13,31 @@ export class ObservableComponentComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit(): void {
-    const observable :Observable<number> = Observable.create((observer: any)  => {
+    const observable: Observable<number> = Observable.create((observer: any) => {
       let count = 0;
-      setInterval(()=>{
+      setInterval(() => {
         observer.next(count++);
-        
-        if(count === 3) {
+
+        if (count === 3) {
           observer.complete();
         }
 
-        if(count > 5) {
+        if (count > 5) {
           observer.error(new Error("ohh no... count is greater than 5"));
         }
       }, 1000);
     });
 
+    // calling pipe and filter operator
     // calling pipe and then map operator
-    this.subscription = observable.pipe(map((d) => `Round: ${d+1}`)) .subscribe(
-      (v) => console.log(v),
-      (error) => { console.error(error); alert(error) },
-      () => console.log('COMPLETED')
-    );
+    this.subscription = observable.pipe(
+      filter(d => d === 0),
+      map((d) => `Round: ${d + 1}`))
+      .subscribe(
+        (v) => console.log(v),
+        (error) => { console.error(error); alert(error) },
+        () => console.log('COMPLETED')
+      );
   }
 
   ngOnDestroy(): void {
